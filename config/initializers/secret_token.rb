d3d4cd322d95c5084e1601e150488569b2278136
David Garcia Alonso <g.a.david@gmail.com>
2011-09-13 00:28:56 +0200

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MiniPost::Application.config.secret_token = '2e8ae39b01afdf38a8638261e768729535598eab8c4e7b0d525d1c317cb12d7e6ed923b217f69de5b2027404eec5ea1912f19d3f029911aeb6b5b741ef660880'
