3f28b7f9d6b0d0e8d1cdd49cbb52ea8b6084375e
David Garcia Alonso <g.a.david@gmail.com>
2011-10-06 20:31:38 +0200

    Avanzo en html.erb dinamico

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,15 @@
 class PagesController < ApplicationController
   def home
+    @title = "Home"
   end
 
   def contact
+    @title = "Contact"
   end
 
   # Acción about
   def about
+    @title = "About"
   end
 
 end
